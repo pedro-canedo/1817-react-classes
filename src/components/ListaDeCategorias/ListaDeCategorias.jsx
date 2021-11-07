2a439a63002d3d6f2e3f82af284f7622571f28fa
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-11-06 21:30:30 -0300

    Adiconando funcionalidade para categorias
    Listagem

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import "./estilo.css";
 
 class ListaDeCategorias extends Component {
+    _handleEventoInput(e){
+        if(e.key === "Enter"){
+            this.props.adicionarCategoria(e.target.value);
+            e.target.value = "";
+        }
+    }
     render() {
         return(
-                <ul>
-                    <li>Categoria 1</li>
-                    <li>Categoria 1</li>
-                    <li>Categoria 1</li>
-                    <li>Categoria 1</li>
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
                 </ul>
+                <input 
+                className="lista-categorias_input" 
+                type="text" 
+                placeholder="Digite sua categoria"
+                onKeyUp={this._handleEventoInput.bind(this)}
+                />
+            </section>
         );
     }
 }
